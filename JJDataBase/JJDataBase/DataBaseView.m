9d3766f34c281a9d19e98cad8606ef2dbfc818a8
liujiajie <liujiajie@dianping.com>
2018-10-03 16:45:02 +0800

    【添加DataBaseView】

---
+++ JJDataBase/JJDataBase/DataBaseView.m
@@ -0,0 +1,29 @@
+//
+//  DataBaseView.m
+//  JJDataBase
+//
+//  Created by 刘佳杰 on 2018/10/3.
+//  Copyright © 2018年 Jiajie.Liu. All rights reserved.
+//
+
+#import "DataBaseView.h"
+
+@interface DataBaseView ()
+
+@end
+
+@implementation DataBaseView
+
+- (instancetype)initWithFrame:(CGRect)frame {
+    self = [super initWithFrame:frame];
+    if (self) {
+        [self buidupViews];
+    }
+    return self;
+}
+
+- (void)buidupViews {
+    
+}
+
+@end
